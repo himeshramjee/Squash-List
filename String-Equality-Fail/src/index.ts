99b0c4ec1a109806e446a90fb4c958ab6c35db09
Himesh Ramjee <himeshramjee@users.noreply.github.com>
2020-10-23 12:50:44 +0200

    Whoops, dropping the loading of express port from process.env as it's just not needed/used right now. Will add it back along with some package like DotEnv when the need arises.

--- String-Equality-Fail/src/index.ts
+++ String-Equality-Fail/src/index.ts
@@ -1,20 +1,9 @@
 import { app } from "./app";
 
 const start = async () => {
-  if (
-    !process.env.PORT ||
-  ) {
-    throw new Error("Missing environment variables.");
-  } else {
-    console.info(
-      `\n[SquashListProject] Using environment variables: 
-      \tPORT=${process.env.port}
-      \n`
-    );
-  }
 
-  app.listen(process.env.PORT, () => {
-    console.log(`SquashListProject v0.0.0 listening on port ${process.env.PORT}...`);
+  app.listen(6999, () => {
+    console.log(`SquashListProject v0.0.0 listening on port 6999...`);
   });
 };
 
